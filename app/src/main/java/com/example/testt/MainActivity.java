1b6b25bd55cab621d2790b3da26bb6df3725ccba
phoovanat <phoovanat.t@gmail.com>
2019-11-06 20:01:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/testt/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.testt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
